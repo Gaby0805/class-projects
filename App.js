8dc5421ed7e03e2737eec90dc2bcc502eae5d858
Gaby <gabbiebrabo@gmail.com>
2025-04-22 20:58:56 -0400

    testes

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home'
+import { View, Text } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import EditarTarefa from './src/screens/EditTask';
+
+
+const RootStack = createNativeStackNavigator({
+  
+  screenOptions: {
+    headerShown: false
+  },
+  screens: {
+    Home: Home,
+    Edit: EditarTarefa,
+  }
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Home></Home>
-    </View>
-  );
+  return <Navigation  />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
